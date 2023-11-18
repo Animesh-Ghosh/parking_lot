d3a6829c6e7d2c5f04b29b7fce8b78f887be685f
Animesh-Ghosh <animesh.ghosh@tuta.io>
2023-11-19 02:37:47 +0530

    Clean up ParkingLot::Interfaces

--- lib/parking_lot/interfaces/command_line_interface.rb
+++ lib/parking_lot/interfaces/command_line_interface.rb
@@ -11,59 +11,88 @@ class ParkingLot
       def process
         while @last_command != 'exit'
           command = gets.chomp
-          dispatch_parking_lot_command(command)
+          parse_and_dispatch_to_parking_lot_for(command)
           @last_command = command
         end
       end
 
       private
 
-      def dispatch_parking_lot_command(command)
+      def parse_and_dispatch_to_parking_lot_for(command)
         case command.split
         in ['create_parking_lot', lot_size]
-          @parking_lot.create_parking_lot lot_size.to_i
-          puts "Created a parking lot with #{lot_size} slots"
+          create_parking_lot(lot_size)
         in ['park', registration_number, colour]
-          slot_number = @parking_lot.park registration_number, colour
-          if slot_number.nil?
-            puts 'Sorry, parking lot is full'
-          else
-            puts "Allocated slot number: #{slot_number}"
-          end
+          park(registration_number, colour)
         in ['leave', slot_number]
-          @parking_lot.leave slot_number.to_i
-          puts "Slot number #{slot_number} is free"
+          leave(slot_number)
         in ['status']
-          status = @parking_lot.status
-          puts 'Slot No. Registration No'
-          status.each do |slot_number, registration_number|
-            puts "#{slot_number} #{registration_number}"
-          end
-        in ['registration_numbers_for_cars_with_colour', colour]
-          registration_numbers = @parking_lot.registration_numbers_for_cars_with_colour colour
-          if registration_numbers.empty?
-            puts 'Not found'
-          else
-            puts registration_numbers.join(', ')
-          end
-        in ['slot_numbers_for_cars_with_colour', colour]
-          slot_numbers = @parking_lot.slot_numbers_for_cars_with_colour colour
-          if slot_numbers.empty?
-            puts 'Not found'
-          else
-            puts slot_numbers.join(', ')
-          end
+          status
+        in [
+          'registration_numbers_for_cars_with_colour' | 'slot_numbers_for_cars_with_colour' => command,
+          colour
+        ]
+          send(command, colour)
         in ['slot_number_for_registration_number', registration_number]
-          slot_number = @parking_lot.slot_number_for_registration_number registration_number
-          if slot_number.nil?
-            puts 'Not found'
-          else
-            puts slot_number
-          end
+          slot_number_for_registration_number(registration_number)
         in ['exit']
           # noop
         else
           raise NoMethodError
+        end
+      end
+
+      def create_parking_lot(lot_size)
+        @parking_lot.create_parking_lot lot_size.to_i
+        puts "Created a parking lot with #{lot_size} slots"
+      end
+
+      def park(registration_number, colour)
+        slot_number = @parking_lot.park registration_number, colour
+        if slot_number.nil?
+          puts 'Sorry, parking lot is full'
+        else
+          puts "Allocated slot number: #{slot_number}"
+        end
+      end
+
+      def leave(slot_number)
+        @parking_lot.leave slot_number.to_i
+        puts "Slot number #{slot_number} is free"
+      end
+
+      def status
+        status = @parking_lot.status
+        puts 'Slot No. Registration No'
+        status.each do |slot_number, registration_number|
+          puts "#{slot_number} #{registration_number}"
+        end
+      end
+
+      def registration_numbers_for_cars_with_colour(colour)
+        registration_numbers = @parking_lot.registration_numbers_for_cars_with_colour colour
+        if registration_numbers.empty?
+          puts 'Not found'
+        else
+          puts registration_numbers.join(', ')
+        end
+      end
+
+      def slot_numbers_for_cars_with_colour(colour)
+        slot_numbers = @parking_lot.slot_numbers_for_cars_with_colour colour
+        if slot_numbers.empty?
+          puts 'Not found'
+        else
+          puts slot_numbers.join(', ')
+        end
+      end
+
+      def slot_number_for_registration_number(registration_number)
+        slot_number = @parking_lot.slot_number_for_registration_number registration_number
+        if slot_number.nil?
+          puts 'Not found'
+        else
+          puts slot_number
         end
       end
     end
--- lib/parking_lot/interfaces/file_interface.rb
+++ lib/parking_lot/interfaces/file_interface.rb
@@ -11,57 +11,86 @@ class ParkingLot
       def process
         File.open(@filename) do |file|
           file.readlines.each do |line|
-            dispatch_parking_lot_command(line)
+            parse_and_dispatch_to_parking_lot_for(line)
           end
         end
       end
 
       private
 
-      def dispatch_parking_lot_command(line)
+      def parse_and_dispatch_to_parking_lot_for(line)
         case line.split
         in ['create_parking_lot', lot_size]
-          @parking_lot.create_parking_lot lot_size.to_i
-          puts "Created a parking lot with #{lot_size} slots"
+          create_parking_lot(lot_size)
         in ['park', registration_number, colour]
-          slot_number = @parking_lot.park registration_number, colour
-          if slot_number.nil?
-            puts 'Sorry, parking lot is full'
-          else
-            puts "Allocated slot number: #{slot_number}"
-          end
+          park(registration_number, colour)
         in ['leave', slot_number]
-          @parking_lot.leave slot_number.to_i
-          puts "Slot number #{slot_number} is free"
+          leave(slot_number)
         in ['status']
-          status = @parking_lot.status
-          puts 'Slot No. Registration No'
-          status.each do |slot_number, registration_number|
-            puts "#{slot_number} #{registration_number}"
-          end
-        in ['registration_numbers_for_cars_with_colour', colour]
-          registration_numbers = @parking_lot.registration_numbers_for_cars_with_colour colour
-          if registration_numbers.empty?
-            puts 'Not found'
-          else
-            puts registration_numbers.join(', ')
-          end
-        in ['slot_numbers_for_cars_with_colour', colour]
-          slot_numbers = @parking_lot.slot_numbers_for_cars_with_colour colour
-          if slot_numbers.empty?
-            puts 'Not found'
-          else
-            puts slot_numbers.join(', ')
-          end
+          status
+        in [
+          'registration_numbers_for_cars_with_colour' | 'slot_numbers_for_cars_with_colour' => command,
+          colour
+        ]
+          send(command, colour)
         in ['slot_number_for_registration_number', registration_number]
-          slot_number = @parking_lot.slot_number_for_registration_number registration_number
-          if slot_number.nil?
-            puts 'Not found'
-          else
-            puts slot_number
-          end
+          slot_number_for_registration_number(registration_number)
         else
           raise NoMethodError
+        end
+      end
+
+      def create_parking_lot(lot_size)
+        @parking_lot.create_parking_lot lot_size.to_i
+        puts "Created a parking lot with #{lot_size} slots"
+      end
+
+      def park(registration_number, colour)
+        slot_number = @parking_lot.park registration_number, colour
+        if slot_number.nil?
+          puts 'Sorry, parking lot is full'
+        else
+          puts "Allocated slot number: #{slot_number}"
+        end
+      end
+
+      def leave(slot_number)
+        @parking_lot.leave slot_number.to_i
+        puts "Slot number #{slot_number} is free"
+      end
+
+      def status
+        status = @parking_lot.status
+        puts 'Slot No. Registration No'
+        status.each do |slot_number, registration_number|
+          puts "#{slot_number} #{registration_number}"
+        end
+      end
+
+      def registration_numbers_for_cars_with_colour(colour)
+        registration_numbers = @parking_lot.registration_numbers_for_cars_with_colour colour
+        if registration_numbers.empty?
+          puts 'Not found'
+        else
+          puts registration_numbers.join(', ')
+        end
+      end
+
+      def slot_numbers_for_cars_with_colour(colour)
+        slot_numbers = @parking_lot.slot_numbers_for_cars_with_colour colour
+        if slot_numbers.empty?
+          puts 'Not found'
+        else
+          puts slot_numbers.join(', ')
+        end
+      end
+
+      def slot_number_for_registration_number(registration_number)
+        slot_number = @parking_lot.slot_number_for_registration_number registration_number
+        if slot_number.nil?
+          puts 'Not found'
+        else
+          puts slot_number
         end
       end
     end
